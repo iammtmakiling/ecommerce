36f07ec842216bdf2c6901b7d3ed0e4818e2e24a
Kael-Makiling <mrmakiling@up.edu.ph>
2024-07-02 12:34:38 +0800

    ui: navbar

---
+++ oldst-react-app/src/components/organisms/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[8rem] bg-white">
+      <div className="text-3xl font-lequire">Kael Jay</div>
+      <div className="text-lg font-contax mt-[-0.5rem]">electronics</div>
+    </div>
+  );
+};
+
+export default Navbar;
--- oldst-react-app/src/components/pages/HomePage.js
+++ oldst-react-app/src/components/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ProductPage from '../templates/ProductPage';
+import Navbar from '../organisms/Navbar';
+import '../../styles/tailwind.css';
 
 const HomePage = () => (
   <div>
+    <Navbar/>
     <ProductPage />
   </div>
 );
